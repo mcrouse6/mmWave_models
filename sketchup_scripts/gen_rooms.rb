312c44e5489ee94ae01ebcc34fcea2f7077e19b8
Alexander Iansiti <aiansiti@outlook.com>
2016-08-10 12:43:54 -0400

    Gen Rooms works. added dir param to window. Dir must already exist.
    Cannot create dir if none exists, requires a bloated library.

--- sketchup_scripts/gen_rooms.rb
+++ sketchup_scripts/gen_rooms.rb
@@ -4,7 +4,6 @@ require 'sketchup.rb'
 # see any programming errors we may make.
 require 'exporter'
 
-export_path = 'C:/Users/aians/Desktop'
 # Add a menu item to launch our plugin.
 UI.menu("Plugins").add_item("Make Rooms") {
   rooms_prompt
@@ -19,7 +18,7 @@ def draw_rooms(input)
   min_length = input[1].to_f.m
   min_width = input[2].to_f.m
   pt1 = [0,0,0]
-  for i in 0..input[3].to_f
+  for i in 0..input[3].to_f-1
     scaled_height = (input[4].to_f**i)*min_height
     scaled_length = (input[5].to_f**i)*min_length
     scaled_width = (input[6].to_f**i)*min_width
@@ -28,15 +27,16 @@ def draw_rooms(input)
     pt4 = [0,scaled_width,0]
     face = entities.add_face pt1, pt2, pt3, pt4
     face.pushpull -1*scaled_height
-    export File.join(export_path, '#{scaled_length}x#{scaled_width}x#{scaled_height})
+    puts File.join(input[7], "#{scaled_length.to_m}x#{scaled_width.to_m}x#{scaled_height.to_m}.stl")
+    export File.join(input[7], "#{scaled_length.to_m}x#{scaled_width.to_m}x#{scaled_height.to_m}.stl")
     entities.clear!
   end
 end
 
 def rooms_prompt
   SKETCHUP_CONSOLE.show
-  prompts = ["Min Height (Meters)", "Min Length", "Min Width", "Num Rooms", "H Scale", "L Scale", "W Scale"]
-  defaults = ["3", "3", "3", "1", "1", "1", "1"]
+  prompts = ["Min Height (Meters)", "Min Length", "Min Width", "Num Rooms", "H Scale", "L Scale", "W Scale", "Export Path"]
+  defaults = ["3", "3", "3", "1", "1", "1", "1", 'C:/Users/aians/Desktop']
   input = UI.inputbox(prompts, defaults, "How to Gen Rooms")
   draw_rooms input
 end
